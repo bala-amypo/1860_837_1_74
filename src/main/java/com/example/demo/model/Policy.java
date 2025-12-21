9b113d58d40fa402d4639395e23317b74fd3d007
bala-amypo <bala@amypo.in>
2025-12-21 17:27:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Policy.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "policies", uniqueConstraints = @UniqueConstraint(columnNames = "policyNumber"))
+public class Policy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private String policyNumber;
+    private String policyType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Policy() {}
+
+    public Policy(User user, String policyNumber, String policyType,
+                  LocalDate startDate, LocalDate endDate) {
+        this.user = user;
+        this.policyNumber = policyNumber;
+        this.policyType = policyType;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    // getters & setters
+}
